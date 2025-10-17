7aaf5c9ec5fdf3afe13aa859c74daf7b4fb4ef5d
pavithra <rendlapavithra@gmail.com>
2025-10-17 09:56:14 +0530

    Spring Boot

---
+++ src/main/java/com/klef/dev/ClientController.java
@@ -0,0 +1,20 @@
+package com.klef.dev;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/auth")
+public class ClientController {
+@GetMapping("/")
+public String demo() {
+	return "Spring Boot";
+}
+@GetMapping("/demo")
+public String test() {
+	return "Spring Boot Test";
+}
+
+}
---
+++ src/main/java/com/klef/dev/LabSpringBootProjectApplication.java
@@ -0,0 +1,14 @@
+package com.klef.dev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+@SpringBootApplication
+public class LabSpringBootProjectApplication  extends SpringBootServletInitializer{
+
+	public static void main(String[] args) {
+		SpringApplication.run(LabSpringBootProjectApplication.class, args);
+	}
+
+}
